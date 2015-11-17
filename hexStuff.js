42588e3e6a044487dfbce28088ddf3706cff94b8
Stéphane Adam Garnier <seedsodesign@gmail.com>
2015-11-17 11:50:25 +0100

    Create hexStuff.js

---
+++ hexStuff.js
@@ -0,0 +1,39 @@
+var string = "hello world"
+var apple = "\uF8FF"
+
+string.charCodeAt(0).toString(16);
+
+String.prototype.hexEncode = function(){
+    var hex, i;
+
+    var result = "";
+    for (i=0; i<this.length; i++) {
+        hex = this.charCodeAt(i).toString(16);
+        result += ("000"+hex).slice(-4);
+    }
+
+    return result
+}
+
+String.prototype.hexEncode2 = function(){
+    var hex, i;
+
+    var result = "";
+    for (i=0; i<this.length; i++) {
+        hex = this.charCodeAt(i).toString(16);
+        result += "\\u" + ("000"+hex).slice(-4);
+    }
+
+    return result
+}
+
+String.prototype.hexDecode = function(){
+    var j;
+    var hexes = this.match(/.{1,4}/g) || [];
+    var back = "";
+    for(j = 0; j<hexes.length; j++) {
+        back += String.fromCharCode(parseInt(hexes[j], 16));
+    }
+
+    return back;
+}
